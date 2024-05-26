6edf3bb4e613faaa35bc61675ec5ab7cd881191a
Hunaifah <165546968+Hunaifah@users.noreply.github.com>
2024-05-26 14:47:29 +0700

    progress

---
+++ Login page/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+// import User from './Components/landingPage/pageUser'
+import Signup from './Components/LoginSignup/signup';
+
+function App() {
+  return (
+    <Signup />
+  );
+}
+
+export default App;
---
+++ Login page/src/Components/landingPage/pageUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const pageUser = () => {
+    return (
+        <div>
+            hello world
+        </div>
+    )
+}
+
+export default pageUser
